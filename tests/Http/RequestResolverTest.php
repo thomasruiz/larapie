8da292103b15ee1f74b0cd7e054afe22572a13d9
Thomas Ruiz <thomas.ruiz@neoxia.com>
2016-09-02 12:23:42 +0200

    Adding tests for FormRequests

--- tests/Http/RequestResolverTest.php
+++ tests/Http/RequestResolverTest.php
@@ -4,7 +4,12 @@ namespace LarapieTests\Http;
 
 use Illuminate\Config\Repository;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Validation\ValidatesWhenResolved;
 use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidatesWhenResolvedTrait;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Validator;
 use Larapie\Http\ModelResource;
 use Larapie\Http\RequestResolver;
 use LarapieTests\TestCase;
@@ -26,21 +31,119 @@ class RequestResolverTest extends TestCase
 
     public function testResolveRequest()
     {
-        $config = new Repository([
-            'larapie' => [
-                'resources' => [
-                    'resource' => [
-                        'model' => 'Foo',
-                    ],
-                ],
-            ],
-        ]);
+        $config = new Repository($this->defaultConfig([
+            'model' => 'Foo',
+        ]));
 
         $this->request->shouldReceive('route->getName')->andReturn('resource.index');
+
         $resolver = new RequestResolver($this->containerMock, $config);
         $request = $resolver->resolve();
 
         $this->assertSame($this->request, $request);
         $this->assertEquals(new ModelResource([], 'Foo', 'resource'), $resolver->getResource());
+    }
+
+    public function testResolveRequestWithFormRequest()
+    {
+        $config = new Repository($this->defaultConfig([
+            'model'   => 'Foo',
+            'request' => FooRequest::class,
+        ]));
+
+        $this->request->shouldReceive('route->getName')->andReturn('resource.store');
+        $this->containerMock->shouldReceive('make')->once()->with(FooRequest::class)->andReturn(new FooRequest);
+
+        $resolver = new RequestResolver($this->containerMock, $config);
+        $request = $resolver->resolve();
+
+        $this->assertInstanceOf(FooRequest::class, $request);
+    }
+
+    public function testResolveRequestWithFormRequestShouldAuthorizeIt()
+    {
+        $this->expectException(UnauthorizedException::class);
+
+        $config = new Repository($this->defaultConfig([
+            'model'   => 'Foo',
+            'request' => FooRequest::class,
+        ]));
+
+        $this->request->shouldReceive('route->getName')->andReturn('resource.store');
+        $this->containerMock->shouldReceive('make')->once()->with(FooRequest::class)->andReturn(new FooRequest(false));
+
+        $resolver = new RequestResolver($this->containerMock, $config);
+        $request = $resolver->resolve();
+
+        $this->assertInstanceOf(FooRequest::class, $request);
+    }
+
+    public function testResolveRequestWithFormRequestShouldValidateIt()
+    {
+        $this->expectException(ValidationException::class);
+
+        $config = new Repository($this->defaultConfig([
+            'model'   => 'Foo',
+            'request' => FooRequest::class,
+        ]));
+
+        $this->request->shouldReceive('route->getName')->andReturn('resource.store');
+        $this->containerMock->shouldReceive('make')->once()->with(FooRequest::class)->andReturn(new FooRequest(true, false));
+
+        $resolver = new RequestResolver($this->containerMock, $config);
+        $request = $resolver->resolve();
+
+        $this->assertInstanceOf(FooRequest::class, $request);
+    }
+
+    protected function defaultConfig($config)
+    {
+        return [
+            'larapie' => [
+                'resources' => [
+                    'resource' => $config,
+                ],
+            ],
+        ];
+    }
+}
+
+class FooRequest implements ValidatesWhenResolved
+{
+    use ValidatesWhenResolvedTrait;
+
+    private $authorize;
+
+    private $passes;
+
+    public function __construct($authorize = true, $passes = true)
+    {
+        $this->authorize = $authorize;
+        $this->passes = $passes;
+    }
+
+    public function authorize()
+    {
+        return $this->authorize;
+    }
+
+    public function validator()
+    {
+        return new ValidatorStub($this->passes);
+    }
+}
+
+class ValidatorStub extends Validator
+{
+    private $passes;
+
+    public function __construct($passes)
+    {
+        $this->passes = $passes;
+    }
+
+    public function passes()
+    {
+        return $this->passes;
     }
 }
